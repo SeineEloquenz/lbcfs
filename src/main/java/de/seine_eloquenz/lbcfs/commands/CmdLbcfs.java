f1df85ff67e4ddb814c63aba0c617aeb199ca2c5
Alexander Linder <alexander-linder@gmx.de>
2019-03-05 20:23:02 +0100

    Merge version subCommand into CmdLbcfs class

--- src/main/java/de/seine_eloquenz/lbcfs/commands/CmdLbcfs.java
+++ src/main/java/de/seine_eloquenz/lbcfs/commands/CmdLbcfs.java
@@ -2,6 +2,7 @@ package de.seine_eloquenz.lbcfs.commands;
 
 import de.seine_eloquenz.lbcfs.LbcfsPlugin;
 import de.seine_eloquenz.lbcfs.annotations.command.SubCommand;
+import de.seine_eloquenz.lbcfs.annotations.command.SubCommandName;
 import de.seine_eloquenz.lbcfs.command.LbcfsCommand;
 import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.annotation.command.Command;
@@ -10,7 +11,7 @@ import org.bukkit.plugin.java.annotation.command.Command;
  * Prints the version of Lbcfs in chat
  */
 @Command(name = "lbcfs", desc = "Lbcfs main command", permission = "lbcfs.admin")
-@SubCommand(SubCmdVersion.class)
+@SubCommand(CmdLbcfs.Version.class)
 public class CmdLbcfs extends LbcfsCommand {
 
     /**
@@ -24,5 +25,28 @@ public class CmdLbcfs extends LbcfsCommand {
     @Override
     public boolean run(final CommandSender sender, final String[] args) {
         return false;
+    }
+
+    /**
+     * This subcommand prints the version of lbcfs into chat
+     */
+    @SubCommandName("version")
+    public final class Version extends de.seine_eloquenz.lbcfs.command.SubCommand {
+
+        /**
+         * Creates a new SubCommand with the given minimal and maximal parameters
+         * NOTE: Name "?" is reserved for listing of subcommands
+         *
+         * @param plugin plugin this command belongs to
+         */
+        public Version(final LbcfsPlugin plugin) {
+            super(plugin);
+        }
+
+        @Override
+        public boolean run(final CommandSender sender, final String[] args) {
+            this.getPlugin().send(sender, this.getPlugin().getMessage("version") + ": " + this.getPlugin().getDescription().getVersion());
+            return true;
+        }
     }
 }
