b1a9fa1513a061aaaadaf14c1019bc726ee7b47c
Alexander Linder <alexander-linder@gmx.de>
2019-03-06 01:03:04 +0100

    Refactor SubCommand annotation

--- src/main/java/de/seine_eloquenz/lbcfs/annotations/command/SubCommand.java
+++ src/main/java/de/seine_eloquenz/lbcfs/annotations/command/SubCommand.java
@@ -2,23 +2,27 @@ package de.seine_eloquenz.lbcfs.annotations.command;
 
 import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
-import java.lang.annotation.Repeatable;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * Specifies a {@link de.seine_eloquenz.lbcfs.command.SubCommand} that belongs to a {@link de.seine_eloquenz.lbcfs.command.LbcfsCommand}
+ * Specifies the name of a {@link de.seine_eloquenz.lbcfs.command.SubCommand}
  */
 @Documented
 @Target(ElementType.TYPE)
-@Repeatable(SubCommands.class)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface SubCommand {
 
     /**
-     * The class of the {@link SubCommand}
-     * @return the subCommand class
+     * The name of the subCommand
+     * @return name
      */
-    Class value();
+    String name();
+
+    /**
+     * The parent command this subcommand belongs to
+     * @return the class of the parent
+     */
+    Class parentCommand();
 }
